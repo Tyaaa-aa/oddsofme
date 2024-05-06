e80a4c6f6083e8f37c97f676187a742e67ecf166
Tya <sirtya@gmail.com>
2024-05-07 05:15:40 +0800

    [refactor]: Componentize Dark/Light mode buttons

---
+++ components/DarkLightButton.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { Moon, Sun } from "lucide-vue-next"
+const colorMode = useColorMode()
+const toggleColorMode = () => (colorMode.preference = colorMode.preference === "dark" ? "light" : "dark")
+</script>
+<template>
+	<Button class="m-5" aria-label="Toggle Dark Mode" @click="toggleColorMode" v-auto-animate>
+		<Moon class="w-4 h-4" v-if="$colorMode.preference === 'light'" />
+		<Sun class="w-4 h-4" v-else />
+	</Button>
+</template>
