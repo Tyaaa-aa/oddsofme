9a11c939318546a20dae4a3e2a82acc2224e38d9
Tya <sirtya@gmail.com>
2024-05-19 19:36:35 +0800

    [fix]: Hydration error fix

--- components/DarkLightButton.vue
+++ components/DarkLightButton.vue
@@ -4,8 +4,13 @@ const colorMode = useColorMode()
 const toggleColorMode = () => (colorMode.preference = colorMode.preference === "dark" ? "light" : "dark")
 </script>
 <template>
-	<Button class="m-5" aria-label="Toggle Dark Mode" @click="toggleColorMode" v-auto-animate>
-		<Moon class="w-4 h-4" v-if="$colorMode.preference === 'light'" />
-		<Sun class="w-4 h-4" v-else />
-	</Button>
+  <Button class="m-5" aria-label="Toggle Dark Mode" @click="toggleColorMode" v-auto-animate>
+    <ClientOnly>
+      <Moon class="w-4 h-4" v-if="colorMode.preference === 'light'" />
+      <Sun class="w-4 h-4" v-else />
+      <template #fallback>
+        <Sun class="w-4 h-4" />
+      </template>
+    </ClientOnly>
+  </Button>
 </template>
