0a3c747c0be8c6f24b5304678f05acd339303b94
Tya <sirtya@gmail.com>
2024-05-21 07:30:08 +0800

    [feat]: Added page transitions

--- app.vue
+++ app.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import { useGlobalStore } from "@/stores/global"
 useHead({
-  title: "Oddsofme",
-  meta: [
-    {
-      name: "Oddsofme | Find the odds of you",
-      content:
-        "See how unique you are by finding the odds of you. Compare yourself with others and see how you stand out. Share your results with your friends and family.",
-    },
-  ],
-  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
-  bodyAttrs: {
-    class: 'transition-background duration-200 ease-in-out'
-  }
+	title: "Oddsofme",
+	meta: [
+		{
+			name: "Oddsofme | Find the odds of you",
+			content:
+				"See how unique you are by finding the odds of you. Compare yourself with others and see how you stand out. Share your results with your friends and family.",
+		},
+	],
+	link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.svg" }],
+	bodyAttrs: {
+		class: 'transition-background duration-200 ease-in-out'
+	}
 })
 
 // const store = useGlobalStore()
@@ -21,8 +21,19 @@ useHead({
 // })
 </script>
 <template>
-  <NuxtLoadingIndicator color="#FFA42E" />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+	transition: all 200ms;
+}
+
+.page-enter-from,
+.page-leave-to {
+	opacity: 0;
+	scale: 0.9;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: {
-		enabled: true,
+  devtools: {
+    enabled: true,
 
-		timeline: {
-			enabled: true,
-		},
-	},
-	modules: [
-		"nuxt-security",
-		"@nuxtjs/tailwindcss",
-		"shadcn-nuxt",
-		"@nuxtjs/color-mode",
-		"@pinia/nuxt",
-		"@formkit/auto-animate/nuxt",
-	],
-	shadcn: {
-		prefix: '',
-		componentDir: './components/ui'
-	},
-	colorMode: {
-		classSuffix: '',
-		preference: 'light'
-	},
-	security: {
-		headers: {
-			crossOriginEmbedderPolicy: process.env.NODE_ENV === 'development' ? 'unsafe-none' : 'require-corp',
-		},
-	},
+    timeline: {
+      enabled: true,
+    },
+  },
+  modules: [
+    "nuxt-security",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+    "@formkit/auto-animate/nuxt",
+  ],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  },
+  colorMode: {
+    classSuffix: '',
+    preference: 'light'
+  },
+  security: {
+    headers: {
+      crossOriginEmbedderPolicy: process.env.NODE_ENV === 'development' ? 'unsafe-none' : 'require-corp',
+    },
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+	vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag === 'bdt'
+    }
+  }
 })
