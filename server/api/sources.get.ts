69e11e445e2b7d7ba6a3abfe209689b2d81e5cb5
Tya <sirtya@gmail.com>
2024-05-21 08:13:52 +0800

    [feat]: Added sources API endpoint

--- server/api/sources.get.ts
+++ server/api/sources.get.ts
@@ -1,24 +1,24 @@
 import { supabase } from '../supabase'
 
 interface Sources {
-	trait_id: number,
-	source: string,
-	category: string
+  trait_id: number,
+  source: string,
+  category: string
 }
 
 export default defineEventHandler(async (event) => {
-	try {
-		const { data, error } = await supabase
-			.from('questions')
-			.select(`trait_id, source, category`)
-			.order('trait_id', { ascending: true })
+  try {
+    const { data, error } = await supabase
+      .from('questions')
+      .select(`trait_id, source, category`)
+      .order('trait_id', { ascending: true })
 
-		if (error) {
-			console.error('Error fetching sources:', error)
-		} else {
-			return data as Sources[]
-		}
-	} catch (err) {
-		console.error('Error fetching sources:', err)
-	}
+    if (error) {
+      console.error('Error fetching sources:', error)
+    } else {
+      return data as Sources[]
+    }
+  } catch (err) {
+    console.error('Error fetching sources:', err)
+  }
 })
