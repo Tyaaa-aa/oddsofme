aca29db89c6d936a67d33bdeb2b345a9d9838931
Tya <sirtya@gmail.com>
2024-05-21 08:05:46 +0800

    [feat]: Improved home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,37 @@
-<script setup lang="ts">
-const { data: ENVIRONMENT, error } = useFetch("/api/environment")
+<script lang="ts" setup>
+import oombgImg from '@/public/images/oombg.svg'
+const handleStart = async () => await navigateTo('/quiz')
 </script>
-
 <template>
-  <Header />
-  <main class="container mx-auto px-4">
-    Developing
-  </main>
+  <div class="grid place-items-center h-[50dvh]">
+    <div class="grid place-items-center">
+      <PopulationCounter />
+      <h1 class="text-5xl font-bold text-center mt-5">Find out the odds of you</h1>
+      <Button @click="handleStart" class="mt-5">Start</Button>
+      <ClientOnly>
+        <Teleport to="body">
+          <img :src="oombgImg" class="fixed top-0 left-0 w-full h-full opacity-5 z-[-1] origin-center object-cover"
+            alt="Background Image">
+        </Teleport>
+      </ClientOnly>
+    </div>
+  </div>
 </template>
 <style scoped>
-main {
-  margin-top: 25dvh;
+img {
+  animation: fadeIn 200ms;
+  scale: 0.7;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+    scale: 0.6;
+  }
+
+  to {
+    opacity: 5%;
+    scale: 0.7;
+  }
 }
 </style>
